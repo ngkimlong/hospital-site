b52c1c210688f7e5000b15b9d514e460c1fdf587
Development Server <ubuntu@ip-172-31-22-179.us-west-2.compute.internal>
2018-09-05 03:56:00 +0000

    implement patient page

---
+++ resources/views/patient_index.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <title>Hospital Management V1</title>
+
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+      <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+      <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.12.1/bootstrap-table.min.css">
+      <!-- Latest compiled and minified JavaScript -->
+      <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.12.1/bootstrap-table.min.js"></script>
+      
+      <!-- Styles -->
+      <style>
+          html, body {
+              background-color: #fff;
+              color: #636b6f;
+              font-family: 'Nunito', sans-serif;
+              font-weight: 200;
+              height: 100vh;
+              margin: 0;
+          }
+
+          .full-height {
+              height: 100vh;
+          }
+
+          .flex-center {
+              align-items: center;
+              display: flex;
+              justify-content: center;
+          }
+
+          .position-ref {
+              position: relative;
+          }
+
+          .top-right {
+              position: absolute;
+              right: 10px;
+              top: 18px;
+          }
+
+          .content {
+              text-align: center;
+          }
+
+          .title {
+              font-size: 84px;
+          }
+
+          .links > a {
+              color: #636b6f;
+              padding: 0 25px;
+              font-size: 12px;
+              font-weight: 600;
+              letter-spacing: .1rem;
+              text-decoration: none;
+              text-transform: uppercase;
+          }
+
+          .m-b-md {
+              margin-bottom: 30px;
+          }
+      </style>
+  </head>
+  <body>
+    <div>
+      <table id="sample_patient"></table>
+    </div>
+  </body>
+  <script type="text/javascript">
+    axios({
+      method:'get',
+      url:'http://ec2-34-222-58-206.us-west-2.compute.amazonaws.com/api/v1/patient?offset=30',
+      responseType:'application/json',
+      //mode: 'no-cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      //withCredentials: true,
+      //credentials: 'same-origin',
+      
+    })
+    .then(function(response) {
+      console.log('response.data::::',response.data.data)
+      $('#sample_patient').bootstrapTable({
+        columns: [
+          {
+            field: 'id',
+            title: 'ID'
+          }, {
+            field: 'full_name',
+            title: 'Name'
+          },{
+            field:'birth_date',
+            title:'Date of Birth'
+          }, {
+            field: 'email',
+            title: 'Email'
+          },{
+            field: 'phone_no',
+            title: 'Phone'
+          }
+          
+        ],
+        data: response.data.data
+      })
+    });
+  </script>
+    
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/patient', function () {
+    return view('patient_index');
+});
