5a26d088f23c8c4ff04820c0d08a776767500ea4
kptpro <tung.le@robustaeng.com>
2018-09-10 03:48:36 +0000

    .gitignore was fixed.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,27 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+// app.js
 
 require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+import React from 'react';
+import { render } from 'react-dom';
+//import { Router, Route, browserHistory } from 'react-router';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Master from './components/Master';
+import CreateItem from './components/CreateItem';
+/*
+render(
+  <Router history={browserHistory}>
+      <Route path="/" component={Master} >
+        <Route path="/add-item" component={CreateItem} />
+      </Route>
+    </Router>,
+        document.getElementById('example'));
+*/
+render (
+    <BrowserRouter>
+        <div>
+         <Route path="/" component={Master}/>
+         <Route path="/add-item" component={CreateItem}/>
+        </div> 
+    </BrowserRouter>, 
+    document.getElementById('example')  
+);
